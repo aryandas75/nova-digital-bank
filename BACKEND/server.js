3c4849344b8b41e949f3172d8520d8ac55fad1e1
Aryan Das <your-github-aryandas98523@gmail.com>
2026-09-22 12:52:30 +0530

    Prepare application for deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
 connectDB();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=> {
-    console.log("server is running on port 3000")
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`server is running on port ${PORT}`);
+});
--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -10,12 +10,10 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/account", accountRouter);
